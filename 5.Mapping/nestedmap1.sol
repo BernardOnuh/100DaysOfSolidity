a6fa614b01d7a967998adc427d53e8adf4d64819
BernardOnuh <onuhbernard4@gmail.com>
2023-02-09 10:06:21 -0800

    map

---
+++ 5.Mapping/nestedmap1.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract NestedMapping {
+    //Nested mapping ( mapping from address to address to another mapping )
+    mapping(address => mapping(uint => bool)) public nested;
+
+    function get(address _addr1, uint _i) public view returns (bool){
+        //You can get values from a nested mapping
+        //even when it is  not initialized
+        return nested[_addr1][_i];
+    }
+    function set(address _addr1, uint _i,bool _boo) public { nested[_addr1][_i] = _boo;}
+
+    function remove(address _addr1, uint _i) public {
+        delete nested[_addr1][_i];
+    }
+}
